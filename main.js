0a8823095ea3b90822824b353c2f1929aac82811
Emilly Pereira Maia <emillypereira1290@gmail.com>
2021-10-08 02:57:05 -0300

    linkando arquivos

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 
-function criar() {
+function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
